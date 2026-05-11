975134641154f5a8f0918e26f77ed3e682db0feb
Raj Mishra <raj212558@gmail.com>
2026-05-12 02:10:29 +0530

    Updated healthcheck Routes

--- Backend/src/routes/healthcheck.routes.js
+++ Backend/src/routes/healthcheck.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import {healthcheck} from "../controllers/healthckeck.controllers.js"
 
-const router = Router()
+const router = Router();
 
 router.route("/").get(healthcheck);
 
