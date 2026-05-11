0369fb178bb02148ef82d998b94499f096f2642d
Raj Mishra <raj212558@gmail.com>
2026-05-12 01:06:13 +0530

    Update Auth Routes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,28 +1,41 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from '../controllers/auth.controllers.js';
-
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  googleAuthLogin,
+  verifyEmailOTP,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/auth.controllers.js";
 
 const router = Router();
 
 // Register new user
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxcount: 1,
-        },
-    ]),
-registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxcount: 1,
+    },
+  ]),
+  registerUser
 );
 
 // Login user
 router.route("/login").post(loginUser);
 
+// Google OAuth Login
+router.route("/google").post(googleAuthLogin);
+
+// Verify OTP
+router.route("/verify-otp").post(upload.none(), verifyEmailOTP);
+
 // Refresh access token
 router.route("/refresh-token").get(refreshAccessToken);
-
 
 // <=> Secured routes <=>
 
@@ -32,7 +45,9 @@ router.route("/logout").post(verifyJWT, logoutUser);
 // Get new refresh token
 router.route("/refresh-tokens").get(verifyJWT, refreshAccessToken);
 
-
+// Password Reset
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:token").post(resetPassword);
 
 // Exporting all routes
 export default router;
