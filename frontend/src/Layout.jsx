bf3d9e35e4c2bebf3d260a301b49c4a758aa726e
Raj Mishra <raj212558@gmail.com>
2026-05-13 12:21:41 +0530

    refactor(layout): improve application layout structure and routing composition

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
-function Layout() {
+function Layout({ children }) {
   return (
     <>
       <Header />
-      <Outlet />
+      {children || <Outlet />}
       <Footer />
     </>
   );
